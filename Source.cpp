875fc2206259e88d42b72c1fc8018656bac888a4
MohamedA-M-Hassan <anehmr0@gmail.com>
2016-08-13 22:43:10 +0200

    letters Example

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 #include <string>
+#include <algorithm>  
 using namespace std;
 int main()
 {
+	string theLine;
+	string arr[26];
+	getline (cin,theLine);
+	cout << '\n' ;
+	// remove white spaces
+	//remove_if(theLine.begin(), theLine.end(), isspace);
+	//cout <<  theLine << endl << endl;
+	theLine.erase(remove_if(theLine.begin(), theLine.end(), isspace), theLine.end());
+	cout <<  theLine << endl << endl;
+	// http://stackoverflow.com/questions/83439/remove-spaces-from-stdstring-in-c
+	transform(theLine.begin(), theLine.end(), theLine.begin(), ::tolower);
+	cout << theLine << endl ;
+
+	for (int i = 0; i< theLine.length();i++)
+		{
+			int k=0; // flag to the index array as many elemnts would be empty if we use "i" 
+			bool flag= true;
+			for (int j = 0; j < 26; j++)
+			{
+				if (theLine.substr(i,1)== arr[j])
+					{flag = false;
+					 break;}
+			}
+			if (flag)
+			{
+				arr[k] = theLine.substr(i,1);
+				k++;
+			}	
+		}
+	if (arr[25].empty() )
+	{
+		cout << "done"<< endl;
+	}
+	else cout << "nope" << endl;
+}
+
+/*
 	// size of datatypes
 	cout << sizeof(char)<<endl ;
 	cout << sizeof(int)<<endl;
@@ -19,7 +57,7 @@ int main()
 	cout << name << endl;
 
 
-		// take the input from the user
+		//********* take the input from the user
 		//cin >> name;
 		//cout << name << endl;
 
@@ -27,7 +65,7 @@ int main()
 		int i = name.length();
 		cout << name << "  " << i <<endl;
 		
-
+		// **** substring and find functions 
 		string str="We think in generalities, but we live in details.";		// (quoting Alfred N. Whitehead)
 
 		string str2 = str.substr (3,5);			   // "think"
@@ -40,6 +78,23 @@ int main()
 
 		  std::cout << str2 << '\n' << str4 << '\n';
 
+		// initials 
+		  cout << "Please enter your full name (first middle last): ";
+
+		 string first;
+
+		string middle;
+
+		string last;
+
+		cin >> first >> middle >> last;
+
+		string initials = first.substr(0, 1) + middle.substr(0, 1) + last.substr(0, 1);
+
+		cout << "Your initials are " << initials << "\n";
+		/*
+
+		
 		/*
 	for (int i = 0; i < 7; i++)
 	{
@@ -49,9 +104,6 @@ int main()
 		}
 		cout << endl;
 	}*/
-}
-
-
 	
 		/*cout << "please enter your name";
 		string name;
